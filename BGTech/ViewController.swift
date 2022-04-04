7cae3376dfe6f8469183fb61d7d38ef11fcd0555
furkanahi <37273678+furkanahi@users.noreply.github.com>
2022-04-04 10:50:21 +0300

    Initial Commit

---
+++ BGTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BGTech
+//
+//  Created by Furkan Ahi on 4.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
