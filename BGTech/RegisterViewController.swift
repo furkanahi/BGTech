0b2900907b978962c242ec9480b40476663df264
furkanahi <37273678+furkanahi@users.noreply.github.com>
2022-04-11 11:03:28 +0300

    Login&Register Ekranı

---
+++ BGTech/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  BGTech
+//
+//  Created by Furkan Ahi on 4.04.2022.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextInput: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
--- BGTech/ViewController.swift
+++ BGTech/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //let imageName = "beymen_logo.jpg"
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
 }
-
